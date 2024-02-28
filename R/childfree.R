6c819ea04850e772652341517a61ddf1f59514a3
zpneal <zpneal@msu.edu>
2024-02-28 14:03:58 -0500

    Add files via upload

---
+++ R/childfree.R
@@ -0,0 +1,12 @@
+#' childfree: Generate and harmonize demographic data to study childfree individuals
+#'
+#' @description Reads demographic data from a variety of public data sources,
+#'    extracting and harmonizing variables useful for the study of childfree
+#'    individuals.
+#'
+#'    Data can be generated from:
+#'    * UN Demographic and Health Surveys data using [dhs()]
+#'
+#' @docType package
+#' @name childfree
+NULL
