dd80cad2ab137a2996c933fe77f51265c24d0e1d
zpneal <zpneal@msu.edu>
2024-03-07 15:53:27 -0500

    Add files via upload

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
 .onAttach <- function(lib,pkg) {
   local_version <- utils::packageVersion("childfree")
   packageStartupMessage("N   ■─┬─o   childfree v",local_version)
-  packageStartupMessage(" O    │     Cite: Neal, Z. P. and Neal, J. W., (2024). Childfree: An R package to generate and harmonize")
-  packageStartupMessage("  K   ■─┬─o demographic data to study childfree individuals.")
-  packageStartupMessage("   I    │   Help: type vignette(\"childfree\"); email zpneal@msu.edu; github zpneal/childfree")
-  packageStartupMessage("    D   X   Beta: type devtools::install_github(\"zpneal/childfree\", ref = \"devel\")")
+  packageStartupMessage(" O    │     CITE: Neal, Z. P. and Neal, J. W., (2024). Childfree: An R package for generating")
+  packageStartupMessage("  K   ■─┬─o and harmonizing childfree demographic data. GitHub.")
+  packageStartupMessage("   I    │   HELP: type vignette(\"childfree\"); email zpneal@msu.edu; github zpneal/childfree")
+  packageStartupMessage("    D   X   BETA: type devtools::install_github(\"zpneal/childfree\", ref = \"devel\")")
 }
