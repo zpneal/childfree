dca89e4facd2da5f39ac5cff66eb885ea847221a
zpneal <zpneal@msu.edu>
2024-03-28 11:03:50 -0400

    Add files via upload

--- R/childfree.R
+++ R/childfree.R
@@ -6,6 +6,8 @@
 #'
 #'    Data can be generated from:
 #'    * UN Demographic and Health Surveys data using [dhs()]
+#'    * Michigan State University State of the State data using [soss()]
+#'    * US CDC National Survey of Family Growth data using [nsfg()]
 #'
 #' @docType package
 #' @name childfree
