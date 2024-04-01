e5c3ab92ef48d8eb6a1ee340380381e0180e5387
zpneal <zpneal@msu.edu>
2024-04-01 14:38:26 -0400

    Add files via upload

--- R/childfree.R
+++ R/childfree.R
@@ -1,4 +1,4 @@
-#' childfree: Aggregate and harmonize demographic data to study childfree individuals
+#' childfree: Access and harmonize demographic data to study childfree individuals
 #'
 #' @description Reads demographic data from a variety of public data sources,
 #'    extracting and harmonizing variables useful for the study of childfree
